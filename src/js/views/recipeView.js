dfc268218db61d1d42fa7904395009298421a5ea
ali-mashayekhi <alimashayekhi42@gmail.com>
2023-04-03 00:09:53 +0330

    initial commit

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -42,9 +42,7 @@ class RecipeView extends View {
               <use href="${icons}#icon-check"></use>
             </svg>
             <div class="recipe__quantity">${
-              currentList.quantity
-                ? new Fraction(currentList.quantity).toString()
-                : ''
+              currentList.quantity ? currentList.quantity : ''
             }</div>
             <div class="recipe__description">
               <span class="recipe__unit">${currentList.unit}</span>
